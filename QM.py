bc2e68bae697a236441f1c0010174fbb9cc961a1
Jerome <jerome.esteve@universite-paris-saclay.fr>
2026-01-25 17:12:22 +0000

    Clean version

--- QM.py
+++ QM.py
@@ -266,3 +266,137 @@ def createQueueMonitor(*args):
         print("Someone already is already listening, falling back to simple QueueMonitor")
         return QueueMonitorSimple(*args)
 
+class Job(threading.Thread):
+    def __init__(self, qmprog, blocking=False):
+        """Create a QM job from a QUA program with interactive monitoring"""
+        self.output = widgets.Output()
+        qm_list =  qmm.list_open_qms()
+        qm = qmm.get_qm(qm_list[0])
+        self.output.append_stdout(f"Sending job to {qm.id}...")
+        self.job = qm.queue.add(qmprog)
+        while self.job.status=="loading":
+            time.sleep(0.1)
+        super().__init__()
+        self.output.append_stdout("loaded\n")
+        self.qm = qm
+        self.button_abort = widgets.Button(description='Abort')
+        self.button_abort.on_click(self.abort_clicked)
+        self.job_table = widgets.HTML(value = "")
+        self.show()
+        self.abort = False
+        self.start()
+        if blocking:
+            self.join()
+
+    def get_results(self,*args):
+        handles = [self.job.result_handles.get(arg) for arg in args]
+        return tuple(h.fetch_all(flat_struct=True) for h in handles)
+            
+    def show(self):
+        display(self.button_abort, self.output, self.job_table)
+
+    def abort_clicked(self, button):
+        self.abort = True
+
+    def __getattr__(self, attr):
+        return getattr(self.job,attr)
+
+    def display(self, table):
+        out = "<em>QM job list:</em><table>"
+        for job in table:
+            waiting_time = f"{time.time()-job["time"]:.0f}" if job["time"] else "--"
+            if job["id"]==self.job.id:
+                out += f"""<tr><td><b>{job["status"].capitalize()}</b></td><td><b>{job["id"]}</b></td><td><b>{job["user"] or os.environ["JUPYTERHUB_USER"]}</td><td><b>{waiting_time}s</b></td></tr>"""
+            else:
+                out += f"""<tr><td>{job["status"].capitalize()}</td><td>{job["id"]}</td><td>{job["user"] or "unknown"}</td><td>{waiting_time}s</td></tr>"""
+        out += "</table>"
+        self.job_table.value = out
+
+    def run(self):
+        poller = zmq.Poller()
+        socket_info = context.socket(zmq.SUB)
+        socket_info.connect(f"tcp://{host}:{port2}")
+        socket_info.subscribe("JOBTABLE")
+        poller.register(socket_info, zmq.POLLIN)
+        if self.job.status=="pending":
+            status = {"status":"pending", "time": time.time(), "user":os.environ["JUPYTERHUB_USER"], "id":self.job.id, "qm_id":self.qm.id}
+            socket.send_string("JOB", flags=zmq.SNDMORE)
+            socket.send_json(status)
+        while self.job.status=="pending":
+            self.output.append_stdout(f"Position in queue {self.job.position_in_queue()} \r")
+            evts = dict(poller.poll(timeout=200))
+            if socket_info in evts:
+                topic = socket_info.recv_string()
+                jobtable = socket_info.recv_json()
+                self.display(jobtable)
+            if self.abort:
+                self.job.cancel()
+                self.output.append_stdout("Job has been canceled\n")
+                self.job_table.value = ""
+                return
+        try:
+            self.job = self.job.wait_for_execution(timeout=2)
+        except:
+            self.output.append_stdout("Job has been canceled\n")
+            self.job_table.value = ""
+            return            
+        if self.job.status=="running":
+            self.output.append_stdout("Job is running...               \n")
+            status = {"status":"running", "time": time.time(), "user":os.environ["JUPYTERHUB_USER"], "id":self.job.id, "qm_id":self.qm.id}
+            socket.send_string("JOB", flags=zmq.SNDMORE)
+            socket.send_json(status)
+        while self.job.status=="running":
+            evts = dict(poller.poll(timeout=200))
+            if socket_info in evts:
+                topic = socket_info.recv_string()
+                jobtable = socket_info.recv_json()
+                self.display(jobtable)
+            if self.abort:
+                self.job.halt()
+                self.output.append_stdout("Job has been halted\n")
+                self.job_table.value = ""
+                return
+        self.output.append_stdout("Job has finished\n")
+        self.job_table.value = ""
+
+class JobSimple:
+    def __init__(self, qmprog):
+        """Create a QM job from a QUA program"""
+        qm_list =  qmm.list_open_qms()
+        qm = qmm.get_qm(qm_list[0])
+        print(f"Sending job to {qm.id}")
+        # Send the QUA program to the OPX, which compiles and executes it
+        self.job = qm.queue.add(qmprog)
+        # Wait for job to be loaded
+        while self.job.status=="loading":
+            print("Job is loading...")
+            time.sleep(0.1)
+        # Wait until job is running
+        time.sleep(0.1)
+        status = {"status":"pending", "time": time.time(), "user":os.environ["JUPYTERHUB_USER"], "id":self.job.id, "qm_id":qm.id}
+        socket.send_string("JOB", flags=zmq.SNDMORE)
+        socket.send_json(status)
+        while self.job.status=="pending":
+            q = self.job.position_in_queue()
+            if q>0:
+                print(job.id,"Position in queue",q,end='\r')
+            time.sleep(0.1)
+        self.job=self.job.wait_for_execution()
+        print(f"\nJob {self.job.id} is running")
+        status = {"status":"running", "time": time.time(), "user":os.environ["JUPYTERHUB_USER"], "id":self.job.id, "qm_id":qm.id}
+        socket.send_string("JOB", flags=zmq.SNDMORE)
+        socket.send_json(status)
+    
+    def get_results(self,*args):
+        handles = [self.job.result_handles.get(arg) for arg in args]
+        return tuple(h.fetch_all(flat_struct=True) for h in handles)
+
+    def __getattr__(self, attr):
+        return getattr(self.job,attr) 
+
+    def wait(self):
+        result_handles = self.job.result_handles
+        while result_handles.is_processing():
+            time.sleep(0.5)
+        
+
